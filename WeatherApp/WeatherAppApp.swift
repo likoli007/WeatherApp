00b4b9f1b644a942281d15075a3c9d3868bec631
likoli007 <holubek.alojz@gmail.com>
2023-04-04 09:27:03 +0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Alojz on 2023/4/4.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
