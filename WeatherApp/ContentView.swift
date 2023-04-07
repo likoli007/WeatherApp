e959ad2a7a4b2af5094b87b312017154e8ebc3b5
likoli007 <holubek.alojz@gmail.com>
2023-04-07 18:20:35 +0800

    Added background rectangles, improved some areas

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -24,7 +24,6 @@ struct ContentView: View {
         ZStack{
             backgroundSunshine
             
-
             ZStack {
                 img.offset(x: CGFloat(ofs1), y: -300)
                     .aspectRatio(contentMode: .fill)
@@ -50,7 +49,7 @@ struct ContentView: View {
             ScrollView(.vertical, showsIndicators: false){
                 VStack{
                     cityInformationTop
-                    cityAQI
+                    cityAQI.padding(.bottom, 5)
                     cityHourlyForecast
                     cityDailyForecast
                     UVIndex
@@ -78,30 +77,30 @@ var backgroundSunshine: some View{
 
 var cityInformationTop: some View{
     VStack {
-        Text("Bratislava").font(.system(size: 40, weight: .medium, design: .default))
-        Text("21°").font(.system(size: 64, weight: .thin, design: .default))
-        Text("Cloudy").font(.system(size: 20, weight: .medium, design: .default))
+        Text("Bratislava").shadow(radius: 5.0).font(.system(size: 40, weight: .medium, design: .default))
+        Text("21°").shadow(radius: 5.0).font(.system(size: 64, weight: .thin, design: .default))
+        Text("Cloudy").shadow(radius: 5.0).font(.system(size: 20, weight: .medium, design: .default))
         HStack{
             Text("H: 23°").font(.system(size: 20, weight: .medium, design: .default))
             Text("L: 16°").font(.system(size: 20, weight: .medium, design: .default))
-        }
+        }.shadow(radius: 5.0)
     }.padding().foregroundColor(.white)
 }
 
 var cityAQI: some View{
     VStack {
         HStack{
-            Image(systemName: "aqi.low")
-            Text("AIR QUALITY")
+            Image(systemName: "aqi.low").resizable().frame(width: 15, height:15)
+            Text("AIR QUALITY").font(.system(size: 12, weight: .medium, design: .default))
             Spacer()
-        }.foregroundColor(.gray)
-        Text("72 - Good").frame(maxWidth: .infinity, alignment: .leading)
-        Text("Current AQI (CN) is 72").frame(maxWidth: .infinity, alignment: .leading)
+        }.foregroundColor(.white.opacity(0.6)).padding(.vertical, 5).frame(height:10)
+        Text("72 - Good").shadow(radius: 5.0).font(.system(size: 18, weight: .medium, design: .default)).frame(maxWidth: .infinity, alignment: .leading).padding(.bottom, 3)
+        Text("Current AQI (CN) is 72.").frame(maxWidth: .infinity, alignment: .leading).font(.system(size: 14, weight: .light, design: .default))
         ZStack {
             Rectangle().fill(LinearGradient(colors:[.green,.green, .yellow, .red, .purple, .indigo, Color(red: 0.52, green: 0.0, blue: 0.125), Color(red: 0.52, green: 0.0, blue: 0.125), Color(red: 0.52, green: 0.0, blue: 0.125)], startPoint: .leading, endPoint: .trailing)).frame( height: 6)
             HStack{
                 Spacer()
-                Circle().strokeBorder(.black, lineWidth: 2).frame(width:8).background(Circle().foregroundColor(.white))
+                Circle().strokeBorder(.teal.opacity(0.8), lineWidth: 2).frame(width:8).background(Circle().foregroundColor(.white))
                 Spacer()
                 Spacer()
                 Spacer()
@@ -109,29 +108,58 @@ var cityAQI: some View{
             
         }.frame(height:8)
         Divider()
-        Text("See More").frame(maxWidth: .infinity, alignment: .leading)
-    }.padding().foregroundColor(.white)
+        HStack{
+            Text("See More").frame(maxWidth: .infinity, alignment: .leading).font(.system(size: 16, weight: .medium, design: .default))
+            Spacer()
+            Image(systemName: "chevron.right")
+        }
+    }.padding().foregroundColor(.white).frame(width:350).background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16.0))
 }
 
 var cityHourlyForecast: some View{
     VStack{
-        Text("Higher temperatures expected tomorrow, with a high of 28°")
+        Text("Higher temperatures expected tomorrow, with a high of 28°").frame(maxWidth: .infinity, alignment: .leading).font(.system(size: 14, weight: .light, design: .default))
         Divider()
         ScrollView(.horizontal, showsIndicators: false){
-            HStack(spacing:30){
-                HourlyView(time: "Now", image: "cloud.sun.fill", temp: "21°")
-                HourlyView(time: "15", image: "sun.max.fill", temp: "23°")
-                HourlyView(time: "16", image: "cloud.sun.fill", temp: "20°")
-                HourlyView(time: "17", image: "cloud.sun.fill", temp: "19°")
-                HourlyView(time: "18", image: "cloud.rain.fill", temp: "17°")
-                HourlyView(time: "19", image: "cloud.rain.fill", temp: "16°")
-                HourlyView(time: "19:13", image: "sunset.fill", temp: "Sunset")
-                HourlyView(time: "20", image: "cloud.rain.fill", temp: "16°")
-                HourlyView(time: "21", image: "cloud.moon.fill", temp: "17°")
+            HStack(spacing:25){
+                Group{
+                    HourlyView(time: "Now", image: "cloud.sun.fill", temp: "21°")
+                    HourlyView(time: "15", image: "sun.max.fill", temp: "23°")
+                    HourlyView(time: "16", image: "cloud.sun.fill", temp: "20°")
+                    HourlyView(time: "17", image: "cloud.sun.fill", temp: "19°")
+                    HourlyView(time: "18", image: "cloud.rain.fill", temp: "17°")
+                    HourlyView(time: "19", image: "cloud.rain.fill", temp: "16°")
+                    HourlyView(time: "19:13", image: "sunset.fill", temp: "Sunset")
+                    HourlyView(time: "20", image: "cloud.rain.fill", temp: "16°")
+                    HourlyView(time: "21", image: "cloud.moon.fill", temp: "17°")
+                    HourlyView(time: "22", image: "cloud.moon.fill", temp: "16°")
+                }
+                Group{
+                    HourlyView(time: "23", image: "moon.fill", temp: "16°")
+                    HourlyView(time: "00", image: "moon.fill", temp: "16°")
+                    HourlyView(time: "01", image: "cloud.moon.fill", temp: "17°")
+                    HourlyView(time: "02", image: "moon.fill", temp: "18°")
+                    HourlyView(time: "03", image: "cloud.moon.fill", temp: "20°")
+                    HourlyView(time: "04", image: "cloud.moon.fill", temp: "21°")
+                    HourlyView(time: "05", image: "moon.fill", temp: "20°")
+                    HourlyView(time: "05:46", image: "sunrise.fill", temp: "Sunrise")
+                    HourlyView(time: "06", image: "sun.max.fill", temp: "20°")
+                    HourlyView(time: "07", image: "cloud.sun.fill", temp: "20°")
+                    
+                }
+                Group{
+                    HourlyView(time: "08", image: "sun.max.fill", temp: "19°")
+                    HourlyView(time: "09", image: "sun.max.fill", temp: "20°")
+                    HourlyView(time: "10", image: "sun.max.fill", temp: "18°")
+                    HourlyView(time: "11", image: "cloud.sun.fill", temp: "17°")
+                    HourlyView(time: "12", image: "cloud.rain.fill", temp: "16°")
+                    HourlyView(time: "13", image: "cloud.rain.fill", temp: "15°")
+                    HourlyView(time: "14", image: "cloud.sun.fill", temp: "17°")
+                }
             }
             
         }
-    }.padding(20).foregroundColor(.white)
+    }.padding().foregroundColor(.white).frame(width: 350).background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16.0))
 }
 
 var UVIndex: some View{
@@ -157,25 +185,37 @@ var UVIndex: some View{
 var cityDailyForecast: some View{
     VStack {
         HStack{
-            Image(systemName: "calendar")
-            Text("10-DAY FORECAST")
+            Image(systemName: "calendar").font(.system(size:15))//.frame(width:30, height:30, alignment: .center))//.resizable().frame(width: 15, height:15)
+            Text("10-DAY FORECAST").font(.system(size: 12, weight: .medium, design: .default)).frame(height:30)
             Spacer()
-        }.foregroundColor(.gray)
+        }.foregroundColor(.white.opacity(0.6)).padding(.vertical, 5).frame(height:10)
         
         Group{
+            Divider()
             DailyView(time: "Today", image: "cloud.rain.fill", temp1: 16, temp2: 23, today: true, min: 14, max: 24, currtemp: 21.0)
+            Divider()
             DailyView(time: "Mon", image: "cloud.rain.fill", temp1: 15, temp2: 20, today: false, min: 14, max: 24, currtemp: 21.0)
+            Divider()
             DailyView(time: "Tue", image: "sun.max.fill", temp1: 18, temp2: 24, today: false, min: 14, max: 24, currtemp: 21.0)
+            Divider()
             DailyView(time: "Wed", image: "cloud.sun.fill", temp1: 15, temp2: 22, today: false, min: 14, max: 24, currtemp: 21.0)
+            Divider()
             DailyView(time: "Thu", image: "cloud.rain.fill", temp1: 14, temp2: 17, today: false, min: 14, max: 24, currtemp: 21.0)
+        }
+        Group{
+            Divider()
             DailyView(time: "Fri", image: "sun.max.fill", temp1: 18, temp2: 23, today: false, min: 14, max: 24, currtemp: 21.0)
+            Divider()
             DailyView(time: "Sat", image: "sun.max.fill", temp1: 14, temp2: 23, today: false, min: 14, max: 24, currtemp: 21.0)
+            Divider()
             DailyView(time: "Sun", image: "cloud.sun.fill", temp1: 16, temp2: 23, today: false, min: 14, max: 24, currtemp: 21.0)
+            Divider()
             DailyView(time: "Mon", image: "cloud.fill", temp1: 15, temp2: 21, today: false, min: 14, max: 24, currtemp: 21.0)
+            Divider()
             DailyView(time: "Tue", image: "cloud.bolt.rain.fill", temp1: 14, temp2: 16, today: false, min: 14, max: 24, currtemp: 21.0)
             
         }
-    }.padding()
+    }.padding().frame(width:350).background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16.0))
 }
 
 struct DailyView: View {
@@ -228,10 +268,10 @@ struct HourlyView: View {
     
     var body: some View {
         VStack{
-            Text(time)
-            Image(systemName: image).renderingMode(.original)
-            Text(temp)
-        }
+            Text(time).padding(.bottom, 3).font(.system(size: 14, weight: .medium, design: .default))
+            Image(systemName: image).renderingMode(.original).font(.system(size:20)).frame(width:30, height:30)//.border(.red)//.resizable().scaledToFit().frame(width: 30, height:30)
+            Text(temp).padding(.top, 3).font(.system(size: 16, weight: .bold, design: .default))
+        }.padding(.top, 10)
     }
 }
 
